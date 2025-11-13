5d7cfe22a18dd534ea27543b68b502b53f76d179
Joanna Berteaud <joanna.berteaud@gmail.com>
2025-11-13 10:05:17 -0500

    Adding NE2001

--- RPPcat_MSP.py
+++ RPPcat_MSP.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyne2001
 from PSRDist import Distances as D
 import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
@@ -21,18 +22,22 @@ B = uhf_msps.b.value
 print(L, B)
 
 dpdf, d = D.dist_pdf(RA, DM, L, B)
+dbf = D.dist_bf(RA, DM, L, B)
 
-plt.figure(figsize = [10,10])
+#plt.figure(figsize = [10,10])
 
 for i in range(len(L)):
+    dne2001 = pyne2001.get_dist(L[i], B[i], DM[i])
     popt, pcov = curve_fit(gauss, d[i], dpdf[i], bounds = (0.,100.), p0 = np.array([np.max(dpdf[i]), np.mean(d[i]), np.mean(d[i])]))
-    plt.subplot(3,3,i+1)
-    plt.plot(d[i], dpdf[i])
-    plt.plot(d[i], gauss(d[i], popt[0], popt[1], popt[2]))
+    #plt.subplot(3,3,i+1)
+    plt.axvline(dne2001[0], color = "tab:green")
+    plt.axvline(dbf[i], color = "tab:blue")
+    plt.plot(d[i], dpdf[i], color = "tab:blue")
+    plt.plot(d[i], gauss(d[i], popt[0], popt[1], popt[2]), color = "tab:orange")
     plt.title(names[i])
     plt.xlabel("D [kpc]")
     plt.ylabel("PDF")
-    print names[i], round(popt[1],3), round(popt[2],3)
+    print names[i], round(dne2001[0],4), round(dbf[i],4), round(popt[1],4), round(popt[2],4), round(abs(dne2001[0]-dbf[i])/popt[2],4)
 
 plt.tight_layout()
 plt.savefig("RPPcatMSPs_distPDF_YMW16.pdf")
